142a336c86fa45a6d0577e8b3ecc42e15ff1eb35
Sawyer Newman <sauuyer@gmail.com>
2026-09-03 17:29:56 -0400

    Fix mismatched quote in API_URL

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 /* The only file you edit after deploying. Both pages read it. */
 
 // Paste the /exec URL from Apps Script: Deploy > New deployment > Web app.
-const API_URL = 'https://script.google.com/macros/s/AKfycbyUXBA1-5dsAuYkodT6KW5kucongWKnqeSMk6V60k6f_ILxb9R9-iRJmwXeuKTnjW5K/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyUXBA1-5dsAuYkodT6KW5kucongWKnqeSMk6V60k6f_ILxb9R9-iRJmwXeuKTnjW5K/exec";
 
 // Link to the Google Sheet, shown on the Orders tab of the farm console.
 const SHEET_URL = "https://docs.google.com/spreadsheets/d/1mDeApghBYfamCXnZz__oKxl-CZfb-JzXVHB2LaDgELc/edit?gid=59093425#gid=59093425";
